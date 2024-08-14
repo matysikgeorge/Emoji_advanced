bd79a767f679271da96c986cdc820fd07ca6a4fb
matysikgeorge <matysikgeorge1995@gmail.com>
2024-08-14 17:07:42 +0300

    footer_pangin

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,47 +1,63 @@
-import { data } from "./emoji.js";
+// import { data } from "./emoji.js";
 
-const cardsWrapperNode = document.querySelector(".cards__wrapper");
+// const cardsWrapperNode = document.querySelector(".cards__wrapper");
 
-const inputNode = document.querySelector(".header__input");
-// function карточки => отрисовка шаблона карточки
+// const inputNode = document.querySelector(".header__input");
+// // function карточки => отрисовка шаблона карточки
 
-function createCard(arr) {
-  const card = document.createElement("article"); // создаем тег article в div в документе html
-  card.className = "card"; // присвоение класса
-  card.innerHTML = `<p class="card__symbol">${arr.symbol}</p>
-                    <h3 class="card__title"> ${arr.title}</h3>   
-                    <p class="card__text">${uniqueCard(arr.keywords)}</p>`; // работаем по article
-  return card;
-} // // создание шаблона карточки
+// function createCard(arr) {
+//   const card = document.createElement("article"); // создаем тег article в div в документе html
+//   card.className = "card"; // присвоение класса
+//   card.innerHTML = `<p class="card__symbol">${arr.symbol}</p>
+//                     <h3 class="card__title"> ${arr.title}</h3>   
+//                     <p class="card__text">${uniqueCard(arr.keywords)}</p>`; // работаем по article
+//   return card;
+// } // // создание шаблона карточки
 
-//forEach, function expression create card(элемент из forEach) push с помощью метода append (куда и что) и вызов функции в аргумент данные data}
+// //forEach, function expression create card(элемент из forEach) push с помощью метода append (куда и что) и вызов функции в аргумент данные data}
 
-function cardRender(data) { // - function render Карточек
-  cardsWrapperNode.innerHTML = " ";
-  data.forEach((element) => {
-    const card = createCard(element); // - с помощью переменной card мы обращаемся к функции (createCard на 8 строчке к шаблону) в которой создали шаблон для карточек
-    cardsWrapperNode.append(card); //  (куда добавляем?) в нашу cardsWrapperNode (добавляем что?) - значение card (отсылает к каждому element функции - symbol, title, keywords), которое мы определили выше
-  });
-}
+// function cardRender(data) { // - function render Карточек
+//   cardsWrapperNode.innerHTML = " ";
+//   data.forEach((element) => {
+//     const card = createCard(element); // - с помощью переменной card мы обращаемся к функции (createCard на 8 строчке к шаблону) в которой создали шаблон для карточек
+//     cardsWrapperNode.append(card); //  (куда добавляем?) в нашу cardsWrapperNode (добавляем что?) - значение card (отсылает к каждому element функции - symbol, title, keywords), которое мы определили выше
+//   });
+// }
 
-// function оставить уникальные слова
+// // function оставить уникальные слова
 
-function uniqueCard(str) {
-  const arr = str.split(" ");
-  const uniqueNum = [...new Set(arr)];
-  return uniqueNum.join(" ");
-}
+// function uniqueCard(str) {
+//   const arr = str.split(" ");
+//   const uniqueNum = [...new Set(arr)];
+//   return uniqueNum.join(" ");
+// }
 
-// function по поиску эмодзи
+// // function по поиску эмодзи
 
-function cardFind(event) {
-  let result = event.target.value.toLocaleLowerCase().trim();
-  let newData = data.filter((el) => el.title.toLocaleLowerCase().includes(result) || el.keywords.toLocaleLowerCase().includes(result) );
-  cardRender(newData);
-}
+// function cardFind(event) {
+//   let result = event.target.value.toLocaleLowerCase().trim();
+//   let newData = data.filter((el) => el.title.toLocaleLowerCase().includes(result) || el.keywords.toLocaleLowerCase().includes(result) );
+//   cardRender(newData);
+// }
 
-cardRender(data);
-inputNode.addEventListener("input", cardFind);
+// cardRender(data);
+// inputNode.addEventListener("input", cardFind);
+
+
+
+const footerPaginationNode = document.querySelector(".footer__left-block");
+footerPaginationNode.innerHTML=`
+                                <p class="page">First<p>
+                                <p class="page">1<p>
+                                <p class="page">2<p>
+                                <p class="page">3<p>
+                                <p class="page">4<p>
+                                <p class="page">5<p>
+                                <p class="page">Last<p>
+`
+console.log(footerPaginationNode);
+
+
 
 // let x = "100"
 // let result1 = data.filter((el) => el.title === x);
